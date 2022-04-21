ac46fc74dec47a64cf95fd3dc309cf82a730dd19
Grace Uko <graceuko97@gmail.com>
2022-04-21 16:19:26 +0100

    pointer, string and array evalutions

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of string str
+ * @str: The string to print
+ *
+ * Return: none
+ */
+void puts_half(char *str)
+{
+	int q = 0, i;
+
+	while (*(str + q) != '\0')
+		q++;
+
+	for (i = (q % 2) == 0 ? (q / 2) : (q / 2 + 1); i < q; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
