f2a9bac0dad8769257f20d11f0d0ed65cf442691
Grace Uko <graceuko97@gmail.com>
2022-04-25 00:00:59 +0100

    function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - Cypher chars
+ * @s: The array
+ *
+ * Return: zero
+ */
+char *leet(char *s)
+{
+	int i = 0, j;
+	char l[] = {'a', 'e', 'o', 't', 'l', '\0'};
+	char u[] = {'A', 'E', 'O', 'T', 'L', '\0'};
+	char n[] = {'4', '3', '0', '7', '1', '\0'};
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; l[j] != '\0'; j++)
+			if (s[i] == l[j] || s[i] == u[j])
+				s[i] = n[j];
+		i++;
+	}
+
+	return (s);
+}
