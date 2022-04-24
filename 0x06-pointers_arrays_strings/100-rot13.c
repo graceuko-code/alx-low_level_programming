008d71110bffd88bf163819c0eb947f09185ef6b
Grace Uko <graceuko97@gmail.com>
2022-04-25 00:30:32 +0100

    pointrs array and string advance task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - Cypher chars
+ * @s: The array
+ *
+ * Return: zero
+ */
+char *rot13(char *s)
+{
+	int i = 0, j;
+	char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char u[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; l[j] != '\0'; j++)
+			if (s[i] == l[j])
+			{
+				s[i] = u[j];
+				break;
+			}
+		i++;
+	}
+
+	return (s);
+}
