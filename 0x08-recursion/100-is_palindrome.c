047e935a3adfe5f08920d57049e3e9c62efbedc7
Grace Uko <graceuko97@gmail.com>
2022-04-28 21:23:03 +0100

    recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+int _palindrome(char *s, int l);
+
+/**
+ * is_palindrome - Detects is a string is a plaindrome
+ * @s: The string
+ *
+ * Return: 1 if true or 0 if false
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (_palindrome(s, _strlen_recursion(s)));
+}
+
+/**
+ * _palindrome - Detects is a string is a plaindrome
+ * @s: The string
+ * @l: The lenght of the string
+ *
+ * Return: 1 if true or 0 if false
+ */
+int _palindrome(char *s, int l)
+{
+	if (*s != *(s + l - 1))
+		return (0);
+	else if (l <= 1)
+		return (1);
+	return (_palindrome(++s, l - 2));
+}
+
+/**
+ * _strlen_recursion - return the lengh of a string
+ * @s: The string
+ *
+ * Return: Zero
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(++s));
+}
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,100 @@
+#include "main.h"
+
+char *_strchr(char *s, char c);
+char *_strstr(char *haystack, char *needle);
+char *_strstrstop(char *haystack, char *needle);
+
+/**
+ * wildcmp - Compares two string to find out if they are identical
+ * @s1: The string 1
+ * @s2: The string 2
+ *
+ * Return: 1 if true or 0 if false
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp(++s1, ++s2));
+	else if (*s1 == '\0' && *s2 == '*')
+		return (wildcmp(s1, ++s2));
+	else if (*s2 == '*' && *(s2 +1) == '*')
+		return (wildcmp(s1, ++s2));
+	else if (*s2 == '*' && *(s2 + 1) != *s1)
+		return (wildcmp(++s1, s2));
+	else if (*s2 == '*' && *(s2 + 1) == *s1)
+	{
+		if (_strstr(s1, (s2 + 1)) != NULL)
+			return (wildcmp(strstr(s1, (s2 + 1)), (s2 + 1)));
+		else
+			return (wildcmp(s1, ++s2));
+	}
+
+    return (0);
+}
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: the buffer source
+ * @c: the char to find
+ *
+ * Return: The pointer to first occurency or 0.
+ */
+char *_strchr(char *s, char c)
+{
+	if (*s == '\0')
+		return (0);
+	else if (*s == c)
+		return (s);
+	return (_strchr(++s, c));
+}
+
+/**
+ * _strstr - locates a substring.
+ * @haystack: the buffer source
+ * @needle: the string to find
+ *
+ * Return: The quantity of bytes contents
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	if (*needle == '\0')
+		return (haystack);
+	if (*haystack == '\0')
+		return (0);
+	if (*haystack == *needle)
+	{
+		if (_strstrstop(haystack, needle) != 0)
+			return (_strstrstop(haystack, needle));
+		else
+			return (_strstr(haystack + 1, needle));
+	}
+	else
+		return (_strstr(haystack + 1, needle));
+
+	return (0);
+}
+
+/**
+ * _strstrstop - locates a character in a string.
+ * @haystack: the buffer source
+ * @needle: the char to find
+ *
+ * Return: The pointer to first occurency or 0.
+ */
+char *_strstrstop(char *haystack, char *needle)
+{
+	if (*needle == '\0' && *haystack == '\0')
+		return (0);
+	if (*needle == '\0')
+		return (haystack);
+	if (*haystack == '\0')
+		return (0);
+	if (*haystack == *needle)
+		if (_strstrstop((haystack + 1), (needle + 1)) != 0)
+			return (_strstrstop(haystack + 1, needle + 1));
+
+	return (0);
+}
