f4ed6d38d714b2a8610e01356041cd27d8b283b6
Grace Uko <graceuko97@gmail.com>
2022-05-05 20:37:07 +0100

    malloc and free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_array - create an array of chars
+ * @size: the size for the array
+ * @c: the character for initialize
+ *
+ * Return: Zero
+ */
+char *create_array(unsigned int size, char c)
+{
+unsigned int j;
+char *s;
+
+	if (size == 0)
+		return (NULL);
+
+	s = (char *) malloc(size * sizeof(c));
+	if (s == NULL)
+		return (NULL);
+
+	for (j = 0; j < size; j++)
+		s[j] = c;
+	return (s);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strdup - create a new same string from other
+ * @str: the original string
+ *
+ * Return: The new space of memory to the new string.
+ */
+char *_strdup(char *str)
+{
+unsigned int j = 0, size;
+char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[j] != '\0')
+		j++;
+	size = j + 1;
+	s = (char *) malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+
+	for (j = 0; j < size; j++)
+		s[j] = str[j];
+	return (s);
+}
